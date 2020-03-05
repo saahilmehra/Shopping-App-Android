839fc535b5b0a49d2b528cbd4e13023fe9448c35
saahilmehra <saahilmehra1998@gmail.com>
2020-03-06 00:31:19 +0530

    Added register account for new users. New users can now signup.

--- app/src/main/java/com/saahil/ssshopping/MainActivity.java
+++ app/src/main/java/com/saahil/ssshopping/MainActivity.java
@@ -2,13 +2,34 @@ package com.saahil.ssshopping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin, btnJoinNow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnJoinNow=findViewById(R.id.btnJoinNow);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+        btnJoinNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/saahil/ssshopping/RegisterActivity.java
@@ -0,0 +1,114 @@
+package com.saahil.ssshopping;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText etUsername, etContact, etPassword;
+    Button btnRegister;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        progressDialog=new ProgressDialog(this);
+
+        etUsername=findViewById(R.id.etUsername);
+        etContact=findViewById(R.id.etContact);
+        etPassword=findViewById(R.id.etPassword);
+        btnRegister=findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAccount();
+            }
+        });
+    }
+
+    private void createAccount() {
+        String name=etUsername.getText().toString().trim();
+        String contact=etContact.getText().toString().trim();
+        String password=etPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(name)){
+            etUsername.setError("Required Field!");
+        }
+        else if(TextUtils.isEmpty(contact)){
+            etContact.setError("Required Field!");
+        }
+        else if(TextUtils.isEmpty(password)){
+            etPassword.setError("Required Field!");
+        }
+        else{
+            progressDialog.setTitle("Creating Account");
+            progressDialog.setMessage("Processing...");
+            progressDialog.setCanceledOnTouchOutside(false);
+            progressDialog.show();
+            validatePhoneNumber(name, contact, password);
+        }
+    }
+
+    private void validatePhoneNumber(final String name, final String contact, final String password) {
+        final DatabaseReference databaseReference;
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.child("Users").child(contact).exists()){
+                    HashMap<String, Object> userDataMap=new HashMap<>();
+                    userDataMap.put("Contact", contact);
+                    userDataMap.put("Name", name);
+                    userDataMap.put("Password", password);
+
+                    databaseReference.child("Users").child(contact).updateChildren(userDataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Account successfully created!", Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                                Intent intent=new Intent(RegisterActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                            else{
+                                progressDialog.dismiss();
+                                Toast.makeText(RegisterActivity.this, "Network Error!!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this, "This "+contact+" already exits. Please login!!", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                    Intent intent=new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
